b70be036f9fc3b43ef8e165cfdcfcad5dcc35286
purohit-srashti <srashti.rp.28@gmail.com>
2019-11-29 01:34:47 +0530

    3rd solution

---
+++ Sol3.py
@@ -0,0 +1,13 @@
+def errors():
+    try:
+        a=int(input())
+
+    except Exception as e:
+
+        print(type(e).__name__,e)
+
+    except KeyboardInterrupt as e:
+
+        print(type(e).__name__,e)
+
+errors()
